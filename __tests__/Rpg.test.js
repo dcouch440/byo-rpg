61dbc0a48bdd25987566534e44cfaf2b6398ba25
dcouch440 <dcouch440@gmail.com>
2021-02-03 14:23:38 -0800

    good fail
    Co-authored-by: Taylor Baker <taylorbaker.coding@gmail.com>
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- __tests__/Rpg.test.js
+++ __tests__/Rpg.test.js
@@ -8,4 +8,9 @@ describe('Rpg', () => {
   test("It will create an object that holds the object for our actors", () => {
     expect(rpg).toEqual({actors: {}});
   });
+  
+  test("It will set an Actor object inside the actors object within the Rpg object.", () => {
+    rpg.setActor("Bob");
+    expect(rpg).toEqual(undefined);
+  });
 })
