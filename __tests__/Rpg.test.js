37efb9b0969b176377af391859ec26d63abfb5f6
dcouch440 <dcouch440@gmail.com>
2021-02-03 15:29:57 -0800

    getHealth => good fail
    Co-authored-by: Taylor Baker <taylorbaker.coding@gmail.com>
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- __tests__/Rpg.test.js
+++ __tests__/Rpg.test.js
@@ -8,17 +8,25 @@ describe('Rpg', () => {
   test("It will create an object that holds the object for our actors", () => {
     expect(rpg).toEqual({actors: {}});
   });
-
-  
   test("It will set an Actor object inside the actors object within the Rpg object.", () => {
     rpg.setActor("Bob");
-    const {name, health} = rpg.actors["Bob"];
+  const {name, health} = rpg.actors["Bob"];
     expect(name).toEqual("Bob");
     expect(health).toEqual(400);
   });
-  
   test("It will return an Actor object with Bob as its name and 400 as its health.", () => {
     rpg.setActor("Bob");
     expect(rpg.getActor("Bob")).toEqual({"name":"Bob","health":400});
   });
+  test ("It will return the current value of the given Actors health", () => {
+    rpg.setActor("Bob")
+    rpg.getHealth("Bob")
+    expect(rpg.getHealth("Bob")).toEqual(undefined);
+  })
+  // test("It will decrease the health of an Actor by the given argument value", () => {
+  //   rpg.setActor("Bob")
+  //   rpg.setDamage(200)
+  //   expect()
+  // })
+
 })
