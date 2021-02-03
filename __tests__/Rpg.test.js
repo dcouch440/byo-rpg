2cf78df774d2b4b897c66c4407f8ed46d4f3d59b
dcouch440 <dcouch440@gmail.com>
2021-02-03 15:37:45 -0800

    setDamage => good FAIL
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- __tests__/Rpg.test.js
+++ __tests__/Rpg.test.js
@@ -23,10 +23,10 @@ describe('Rpg', () => {
     rpg.getHealth("Bob")
     expect(rpg.getHealth("Bob")).toEqual(400);
   })
-  // test("It will decrease the health of an Actor by the given argument value", () => {
-  //   rpg.setActor("Bob")
-  //   rpg.setDamage(200)
-  //   expect()
-  // })
+  test("It will decrease the health of an Actor by the given argument value", () => {
+    rpg.setActor("Bob")
+    rpg.setDamage("Bob", 200)
+    expect(rpg.getHealth("Bob")).toEqual(undefined);
 
+  });
 })
