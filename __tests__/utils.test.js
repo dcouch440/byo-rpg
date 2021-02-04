a6299a8e4ab8593e7ca0e67c6772c818333728df
dcouch440 <dcouch440@gmail.com>
2021-02-04 10:28:50 -0800

    checkValid => PASS
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -10,6 +10,6 @@ describe("checkValid", () => {
     expect(checkValid('Bob', rpg)).toEqual('BobJr');
   });
   test("It will check if the key values already exist and return the input value if the value is not found", () => {
-    expect(checkValid('Bob', rpg)).toEqual(undefined)
+    expect(checkValid('Bob', rpg)).toEqual('Bob');
   })
 })
